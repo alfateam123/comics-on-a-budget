bda5b0762e2140de49b22ba3eae4403229fb648a
alfateam123 <alfateam123@gmail.com>
2014-05-28 23:19:05 +0200

    now can insert new books

--- src/calculation.js
+++ src/calculation.js
@@ -5,5 +5,37 @@ TODO: start writing tests
 
 */
 
-var gimmelist = function(comic_list, max_price){
+var Comic = function(comic_name, comic_price){
+	return {name: comic_name, price: comic_price};
 }
+
+var ComicList = {
+
+    comics: [],
+
+    addRawComic: function(name, price){
+    	this.comics.push(Comic(name, price));
+    },
+
+    addComic: function(newcomic){
+    	this.comics.push(newcomic);
+    },
+
+    removeComic: function(name){
+    	this.comics = this.comics.filter(function(comic){return comic.name !== name});
+    },
+
+    removeComicByIndex: function(indexToRemove){
+    	this.comics = this.comics.filter(function(comic, index){return indexToRemove !== index});
+    },
+
+    getAllComics: function(){
+    	return this.comics;
+    },
+
+    whatICanBuy: function(maxPrice){
+    	//RETURNS: a list of indexes (0, comics.length(
+    	return [];
+    }
+}
+
